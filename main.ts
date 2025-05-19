1c54b030e0a569f5e39f327fae87e0b884789984
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2025-05-19 09:52:54 -0700

    Reorganize code structure by grouping constants, global variables, classes, functions, and event handlers

--- main.ts
+++ main.ts
@@ -1,10 +1,33 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    showSprite(sprites.create(list._pickRandom(), SpriteKind.Player))
-})
+/*
+ * Constant variables
+ */
+
+/*
+ * Global variables
+ */
+let list: Image[] = []
+
+/*
+ * Classes
+ */
+
+/*
+ * Functions
+ */
 function showSprite (mySprite: Sprite) {
     mySprite.setPosition(randint(0, scene.screenWidth() - 8), randint(0, scene.screenHeight() - 8))
 }
-let list: Image[] = []
+
+/*
+ * Event handlers
+ */
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    showSprite(sprites.create(list._pickRandom(), SpriteKind.Player))
+})
+
+/*
+ *Main program
+ */
 list = [
 assets.image`myCat`,
 assets.image`mySnake`,
